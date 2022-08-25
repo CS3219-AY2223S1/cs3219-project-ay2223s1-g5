1c981292aab9a7b1ffb1d05c669035fc45a8c95f
Dexter Tan <dev@dextertanyj.com>
2022-08-25 21:38:45 +0800

    chore: setup frontend with typescript

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['build', 'tsconfig.json', '.eslintrc.js'],
+  env: { es6: true },
+  root: true,
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', ignoreRestSiblings: true },
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+}
