7aaaa86a0ee9ab5854bed5d2adfe37a7773595aa
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2022-08-27 20:46:04 +0800

    fix: frontend linter

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,43 +1,45 @@
 module.exports = {
-  plugins: [
-    '@typescript-eslint',
-    'react',
-    'react-hooks',
-  ],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['build', 'tsconfig.json', '.eslintrc.js'],
+  ignorePatterns: ["build", "tsconfig.json", ".eslintrc.js"],
   env: { es6: true },
   root: true,
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   rules: {
-    'no-console': 'warn',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      { argsIgnorePattern: '^_', ignoreRestSiblings: true },
+    "no-console": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", ignoreRestSiblings: true },
     ],
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
       {
         multiline: {
-          delimiter: 'none',
+          delimiter: "none",
           requireLast: true,
         },
         singleline: {
-          delimiter: 'semi',
+          delimiter: "semi",
           requireLast: false,
         },
       },
     ],
-    'react/react-in-jsx-scope': 'off',
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
-}
+};
