59168525da2a2fb0f79380e804f3cabc21a67b67
Dexter Tan <dev@dextertanyj.com>
2022-09-04 22:10:15 +0800

    feat: implement user creation hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import { Login } from "./pages/Login";
 // import { PasswordReset } from "./pages/PasswordReset";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <Login />
-    </div>
+    </QueryClientProvider>
   );
 };
 
---
+++ frontend/src/hooks/useUsers.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "react-query";
+
+import { ApiService } from "src/services/ApiService";
+
+import { CreateUserReq } from "~shared/types/api/user.dto";
+
+export const useCreateUser = () => {
+  const createUser = async (input: CreateUserReq) => {
+    await ApiService.post<void>(`/users`, input);
+  };
+  const {
+    isSuccess: isCreateUserSuccess,
+    isLoading: isCreateUserLoading,
+    mutateAsync: createUserMutation,
+  } = useMutation(createUser);
+  return {
+    createUserMutation,
+    isCreateUserSuccess,
+    isCreateUserLoading,
+  };
+};
