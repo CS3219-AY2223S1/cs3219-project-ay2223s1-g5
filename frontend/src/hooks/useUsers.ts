07102da70ac3c02fbfd50df4dc81c64de282859a
Dexter Tan <dev@dextertanyj.com>
2022-09-09 09:14:01 +0800

    refactor: remove unused return

--- frontend/src/hooks/useUsers.ts
+++ frontend/src/hooks/useUsers.ts
@@ -8,14 +8,10 @@ export const useCreateUser = () => {
   const createUser = async (input: CreateUserReq) => {
     await ApiService.post<void>(`/users`, input);
   };
-  const {
-    isSuccess: isCreateUserSuccess,
-    isLoading: isCreateUserLoading,
-    mutateAsync: createUserMutation,
-  } = useMutation(createUser);
+  const { isLoading: isCreateUserLoading, mutateAsync: createUserMutation } =
+    useMutation(createUser);
   return {
     createUserMutation,
-    isCreateUserSuccess,
     isCreateUserLoading,
   };
 };
