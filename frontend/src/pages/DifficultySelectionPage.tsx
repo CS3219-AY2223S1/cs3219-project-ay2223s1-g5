39679ceaeda77103b73f6ab500043276256c1ed3
cwenling <e0425906@u.nus.edu>
2022-09-17 16:49:58 +0800

    feat: add skeleton for difficulty selection page

--- frontend/src/pages/DifficultySelectionPage.tsx
+++ frontend/src/pages/DifficultySelectionPage.tsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, Stack, Typography } from "@mui/material";
+
+import { DifficultyLevel } from "~shared/types/base";
+
 export const DifficultySelectionPage = () => {
-  return <div>Difficulty Selection Page</div>;
+  const navigate = useNavigate();
+  const [difficultyLevel, setDifficultyLevel] = useState("");
+  const handleChangeDifficulty = (difficultyLevel: DifficultyLevel) => {
+    setDifficultyLevel(difficultyLevel);
+  };
+
+  return (
+    <Box>
+      <Stack>
+        <Typography>Select Difficulty</Typography>
+
+        <Button
+          onClick={() => {
+            handleChangeDifficulty(DifficultyLevel.EASY);
+            navigate("/waiting-page");
+          }}
+        >
+          Easy
+        </Button>
+
+        <Button
+          onClick={() => {
+            handleChangeDifficulty(DifficultyLevel.MEDIUM);
+            navigate("/waiting-page");
+          }}
+        >
+          Medium
+        </Button>
+
+        <Button
+          onClick={() => {
+            handleChangeDifficulty(DifficultyLevel.HARD);
+            navigate("/waiting-page");
+          }}
+        >
+          Hard
+        </Button>
+      </Stack>
+    </Box>
+  );
+  // return <div>Difficulty Selection Page</div>;
 };
