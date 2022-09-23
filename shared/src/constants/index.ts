5fa77c532bea013f27ab9b6cd29bf2dbe6a1f9eb
Dexter Tan <dev@dextertanyj.com>
2022-09-24 00:03:09 +0800

    refactor: remove unused constant

--- shared/src/constants/index.ts
+++ shared/src/constants/index.ts
@@ -1,3 +1,3 @@
-export { EDITOR_DOCUMENT_NAME, JWT_COOKIE_NAME } from "./constants";
+export { EDITOR_DOCUMENT_NAME } from "./constants";
 export { QUEUE_EVENTS, ROOM_EVENTS } from "./events";
 export { QUEUE_NAMESPACE, ROOM_NAMESPACE } from "./namespaces";
