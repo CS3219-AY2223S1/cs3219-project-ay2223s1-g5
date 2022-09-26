a355212372af65a54310f7c74a23d073b6ff326a
Dexter Tan <dev@dextertanyj.com>
2022-09-26 11:40:24 +0800

    fix: set trust proxy for secure cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { Logger } from "nestjs-pino";
 
 import { SocketSessionAdapter } from "src/common/adapters/websocket.adapter";
@@ -7,13 +8,14 @@ import { ConfigService } from "src/core/config/config.service";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true,
   });
 
   app.useLogger(app.get(Logger));
   app.setGlobalPrefix("/api");
   app.useWebSocketAdapter(new SocketSessionAdapter(app));
+  app.set("trust proxy", true);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get("port"));
