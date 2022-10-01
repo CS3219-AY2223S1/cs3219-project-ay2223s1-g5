02be3f50a28e0c8c6c2d261e5628024fe7f12910
cwenling <e0425906@u.nus.edu>
2022-10-02 01:23:20 +0800

    feat: get random question by difficulty

--- backend/src/question/question.service.ts
+++ backend/src/question/question.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { Difficulty, Question } from "@prisma/client";
 
 import { PrismaService } from "src/core/prisma.service";
+
+import { DifficultyLevel } from "../../../shared/src/types/base";
 
 @Injectable()
 export class QuestionService {
   constructor(private prisma: PrismaService) {}
+
+  // gets a random question of diffiulty difficultyLevel
+  async getByDifficulty(
+    difficultyLevel: DifficultyLevel,
+  ): Promise<Question | null> {
+    const difficulty: Difficulty = difficultyLevel;
+    const questionsCount = await this.prisma.question.count({
+      where: { difficulty: difficulty },
+    });
+    const randomNum = Math.floor(Math.random() * questionsCount);
+    return this.prisma.question.findFirst({
+      take: 1,
+      skip: randomNum,
+      where: { difficulty: difficulty },
+    });
+  }
 }
