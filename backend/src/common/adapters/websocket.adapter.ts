4877a4be8f704ecf0ab445ea6fcf9b531085ea43
Dexter Tan <dev@dextertanyj.com>
2022-10-03 17:31:25 +0800

    feat: guard editor websocket connection

--- backend/src/common/adapters/websocket.adapter.ts
+++ backend/src/common/adapters/websocket.adapter.ts
@@ -3,7 +3,7 @@ import { IoAdapter } from "@nestjs/platform-socket.io";
 import { Request, Response } from "express";
 import passport from "passport";
 import { Server, ServerOptions, Socket } from "socket.io";
-import { ExtendedError } from "socket.io/dist/namespace";
+import { ExtendedError, Namespace } from "socket.io/dist/namespace";
 
 import { SessionMiddleware } from "src/common/middlewares/SessionMiddleware";
 import { ConfigService } from "src/core/config/config.service";
@@ -12,6 +12,58 @@ import { RedisService } from "src/redis/redis.service";
 export const session = (socket: Socket): Request["session"] => {
   return (socket.request as Request).session;
 };
+
+export const wrap =
+  (middleware: NestMiddleware) =>
+  (socket: Socket, next: (error?: ExtendedError) => void) =>
+    middleware.use(socket.request, {}, next);
+
+export function serverMiddlewareSetup(
+  server: Server,
+  middleware: NestMiddleware,
+): Server;
+export function serverMiddlewareSetup(
+  server: Namespace,
+  middleware: NestMiddleware,
+): Namespace;
+
+export function serverMiddlewareSetup(
+  server: Server | Namespace,
+  middleware: NestMiddleware,
+): Server | Namespace {
+  server.use(wrap(middleware));
+  server.use((socket: Socket, next: (e?: ExtendedError) => void) => {
+    return passport.initialize()(
+      socket.request as Request,
+      {} as Response,
+      (e?: Error | "router" | "route") => {
+        if (typeof e === "string") {
+          // passport.initialize only ever calls next()
+          // so this should be safe.
+          next();
+          return;
+        }
+        next(e);
+      },
+    );
+  });
+  server.use((socket: Socket, next: (e?: ExtendedError) => void) => {
+    return passport.session()(
+      socket.request as Request,
+      {} as Response,
+      (e?: ExtendedError) => next(e),
+    );
+  });
+  server.use((socket: Socket, next: (error?: ExtendedError) => void) => {
+    const request = socket.request as Request;
+    if (request.isAuthenticated()) {
+      next();
+    } else {
+      next(new Error("unauthorized"));
+    }
+  });
+  return server;
+}
 
 export class SocketSessionAdapter extends IoAdapter {
   constructor(private readonly context: INestApplication) {
@@ -28,47 +80,12 @@ export class SocketSessionAdapter extends IoAdapter {
     },
   ): Server {
     const server = super.create(port, options) as Server;
-    const wrap =
-      (middleware: NestMiddleware) =>
-      (socket: Socket, next: (error?: ExtendedError) => void) => {
-        middleware.use(socket.request, {}, next);
-      };
 
     const middleware = new SessionMiddleware(
       this.context.get(ConfigService),
       this.context.get(RedisService),
     );
-    server.use(wrap(middleware));
-    server.use((socket: Socket, next: (e?: ExtendedError) => void) => {
-      passport.initialize()(
-        socket.request as Request,
-        {} as Response,
-        (e?: Error | "router" | "route") => {
-          if (typeof e === "string") {
-            // passport.initialize only ever calls next()
-            // so this should be safe.
-            next();
-            return;
-          }
-          next(e);
-        },
-      );
-    });
-    server.use((socket: Socket, next: (e?: ExtendedError) => void) =>
-      passport.session()(
-        socket.request as Request,
-        {} as Response,
-        (e?: ExtendedError) => next(e),
-      ),
-    );
-    server.use((socket: Socket, next: (error?: ExtendedError) => void) => {
-      const request = socket.request as Request;
-      if (request.isAuthenticated()) {
-        next();
-      } else {
-        next(new Error("unauthorized"));
-      }
-    });
-    return server;
+
+    return serverMiddlewareSetup(server, middleware);
   }
 }
