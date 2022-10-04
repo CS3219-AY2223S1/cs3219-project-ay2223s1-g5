289f139ae2e2bbeafaca0444067970c92d70020c
cwenling <e0425906@u.nus.edu>
2022-10-04 17:32:04 +0800

    fix: resolve comments

--- backend/src/question/question.service.ts
+++ backend/src/question/question.service.ts
@@ -3,14 +3,14 @@ import { Category, Question, Topic } from "@prisma/client";
 
 import { PrismaService } from "src/core/prisma.service";
 
-import { DifficultyLevel } from "../../../shared/src/types/base";
+import { DifficultyLevel } from "~shared/types/base";
 
 @Injectable()
 export class QuestionService {
   constructor(private prisma: PrismaService) {}
 
   async getQuestionById(id: number): Promise<Question | null> {
-    return this.prisma.question.findFirst({
+    return this.prisma.question.findUnique({
       where: { id: id },
     });
   }
@@ -40,11 +40,23 @@ export class QuestionService {
   }
 
   // gets a random question number of diffiulty difficultyLevel
-  async getIdByDifficulty(difficultyLevel: DifficultyLevel): Promise<number> {
+  async getIdByDifficulty(
+    difficultyLevel: DifficultyLevel,
+  ): Promise<number | null> {
     const questionsCount = await this.prisma.question.count({
       where: { difficulty: difficultyLevel },
     });
-    const randomQuestionId = Math.floor(Math.random() * questionsCount); // 0 if no questions
-    return randomQuestionId;
+    const randomNum = Math.floor(Math.random() * questionsCount);
+    const question = await this.prisma.question.findFirst({
+      take: 1,
+      skip: randomNum,
+      where: { difficulty: difficultyLevel },
+      select: { id: true },
+    });
+    if (question === null) {
+      return 0;
+    }
+    const { id } = question;
+    return id;
   }
 }
